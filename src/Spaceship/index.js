37bfce8468c2a38eebecdd89b79222a9025a76b5
이용재 <dididy@knou.ac.kr>
2019-10-25 20:24:26 +0900

    Fix: accept logic for roket launch

--- src/Spaceship/index.js
+++ src/Spaceship/index.js
@@ -1,76 +1,76 @@
-import styled, {keyframes} from 'styled-components'
-import React from 'react'
+import styled, { keyframes } from "styled-components";
+import React from "react";
 
-const openLeftLeg = keyframes`
+const closeLeftLeg = keyframes`
   from {
-    transform: rotate(90deg) skewX(35deg); }
-  to {
     transform: rotate(-35deg) skewX(-35deg); } 
-`
+    to {
+    transform: rotate(90deg) skewX(35deg); }
+`;
 
-const openRightLeg = keyframes`
+const closeRightLeg = keyframes`
   from {
-    transform: rotate(-90deg) skewX(-35deg); }
+    transform: rotate(35deg) skewX(-35deg); }
   to {
-    transform: rotate(35deg) skewX(35deg); }
-`
+    transform: rotate(-90deg) skewX(35deg); }
+`;
 
 const wiggle = keyframes`
   0% {
     left: calc(90% - 50px);
     top: calc(50% - 50px); }
-  50% {
+  5% {
     left: calc(90% - 45px);
     top: calc(50% - 48px); }
-  100% {
+  10% {
+    left: calc(90% - 50px);
+    top: calc(50% - 50px);  }
+  15% {
+    left: calc(90% - 45px);
+    top: calc(50% - 48px); }
+  20% {
     left: calc(90% - 50px);
     top: calc(50% - 50px); } 
-`
+`;
 
-const land = keyframes`
+const takeoff = keyframes`
   from {
-    top: -260px; 
+    top: calc(100% - 180px);
   }
   to {
-    top: calc(100% - 180px);
+    top: -400px; 
     display: none; 
   }
-`
+`;
 
 const engineTurnedOn = keyframes`
   0% {
     transform-origin: top center;
-    transform: scale(1); }
+    transform: scale(0); }
   50% {
+    transform-origin: top center;
+    transform: scale(0.5); }
+  60% {
     transform-origin: top center;
     transform: scale(0.8); }
   100% {
     transform-origin: top center;
     transform: scale(1); } 
-`
+`;
 
-const engineTurnOff = keyframes`
-  0% {
-    transform-origin: top center;
-    transform: scale(1); }
-  100% {
-    transform-origin: top center;
-    transform: scale(0); }
-`
-
-const Rocket = styled.div`
-  display: ${props => (props.display ? 'block' : 'none')};
+const RocketTakeoff = styled.div`
+  display: ${props => (props.display ? "block" : "none")};
   position: absolute;
   left: calc(90% - 50px);
-  top: calc(50% - 50px);
-  height: 220px;
-  animation-name: ${wiggle}, none, ${land};
-  animation-duration: 320ms, 2000s, 2000ms;
-  animation-delay: 0ms, 3000ms;
-  animation-iteration-count: 8, 1,1;
+  top: calc(100% - 180px);
+  height: 160px;
+  animation-name: ${wiggle}, none, ${takeoff};
+  animation-duration: 1000ms, 500s, 2000ms;
+  animation-delay: 1000ms;
+  animation-iteration-count: 8, 1, 1;
   animation-timing-function: ease-out;
-  animation-fill-mode: none,none,forwards;
-`
+  animation-fill-mode: none, none, forwards;
+`;
 
 const RocketSpan = styled.span`
   display: block;
@@ -84,8 +84,8 @@ const RocketSpan = styled.span`
   border-bottom: 3px solid #191919;
   box-sizing: initial;
 
-  :after{ 
-    content: '';
+  :after {
+    content: "";
     position: absolute;
     display: block;
     left: 10px;
@@ -93,10 +93,10 @@ const RocketSpan = styled.span`
     height: 100%;
     background-color: #d6d8e1;
   }
-`
+`;
 
 const RocketBow = styled.i`
-  top: 0;
+  top: 100%;
   height: 60px;
   width: 26px;
   display: block;
@@ -106,8 +106,8 @@ const RocketBow = styled.i`
   box-sizing: border-box;
   border-left: 3px solid #797d88;
 
-  :after{
-    content: '';
+  :after {
+    content: "";
     position: absolute;
     display: block;
     left: 13px;
@@ -115,9 +115,9 @@ const RocketBow = styled.i`
     top: 3px;
     height: 27px;
     border-top-right-radius: 100%;
-    background: linear-gradient(180deg, #a7a9b1 20%, #d6d8e1); 
+    background: linear-gradient(180deg, #a7a9b1 20%, #d6d8e1);
   }
-`
+`;
 
 const Fin = styled.i`
   display: block;
@@ -127,17 +127,17 @@ const Fin = styled.i`
   position: absolute;
   top: 20px;
   background-color: #3a3a3b;
-`
+`;
 
 const FinLeft = Fin.extend`
   border-top-left-radius: 100%;
   left: -7px;
-`
+`;
 
 const FinRight = Fin.extend`
   border-top-right-radius: 100%;
   right: -7px;
-`
+`;
 
 const RocketEngine = styled.i`
   width: 100%;
@@ -146,90 +146,88 @@ const RocketEngine = styled.i`
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
   position: absolute;
-  background-color: #3a3a3b; 
-`
+  background-color: #3a3a3b;
+`;
 
 const Leg = styled.i`
   position: absolute;
-  display: block; 
-  bottom: 65px;
+  display: block;
+  bottom: 25px;
   width: 35px;
   height: 7px;
   background-color: #3a3a3b;
-`
+`;
 
-const LegLeft = Leg.extend`
+const LegLeftTakeoff = Leg.extend`
   right: 23px;
   transform-origin: right center;
-  animation-name: ${openLeftLeg};
-  animation-duration: 2s;
+  animation-name: ${closeLeftLeg};
+  animation-duration: 500ms;
   animation-delay: 500ms;
   animation-fill-mode: forwards;
-  transform: rotate(90deg) skewX(35deg);
-`
+  transform: rotate(-35deg) skewX(-35deg);
+`;
 
-const LegRight = Leg.extend`
+const LegRightTakeoff = Leg.extend`
   left: 23px;
   transform-origin: left center;
-  animation: ${openRightLeg};
-  animation-duration: 2s;
+  animation: ${closeRightLeg};
+  animation-duration: 500ms;
   animation-delay: 500ms;
   animation-fill-mode: forwards;
-  transform: rotate(-90deg) skewX(-35deg);
-`
+  transform: rotate(35deg) skewX(-35deg);
+`;
 
-const Blaze = styled.i`
+const BlazeTakeoff = styled.i`
   position: absolute;
   top: 160px;
   left: -6px;
   display: block;
-  background-color: #FF3D00;
+  background-color: #ff3d00;
   border-radius: 50% 50% 70% 70%;
   width: 40px;
   height: 60px;
-  animation-name: ${engineTurnedOn}, ${engineTurnOff};
-  animation-duration: 200ms, 1000ms;
-  animation-delay: 0ms, 2000ms;
-  animation-iteration-count: infinite, 1;
-  animation-fill-mode: none, forwards; 
-`
+  animation-name: ${engineTurnedOn};
+  animation-duration: 3500ms;
+  animation-delay: 0ms;
+  animation-iteration-count: 1, infinite;
+  animation-fill-mode: forwards, none;
+`;
 
 const BlazeInner = styled.i`
   position: absolute;
   display: block;
   left: 5px;
   border-radius: 50% 50% 90% 90%;
-`
+`;
 
 const BlazeInnerMedium = BlazeInner.extend`
   width: 30px;
   height: 45px;
-  background-color: #FFD600; 
-`
+  background-color: #ffd600;
+`;
 
 const BlazeInnerExtra = BlazeInner.extend`
   left: 12px;
   width: 15px;
   height: 30px;
-  background-color: #FFFF00;
-`
+  background-color: #ffff00;
+`;
 
-const Spaceship = ({display, onAnimationEnd}) => (
-  <Rocket display={display} onAnimationEnd={onAnimationEnd.bind(this)}>
-    <RocketSpan/>
-    <RocketBow/>
-    <FinLeft/>
-    <FinRight/>
-    <RocketEngine/>
-    <LegLeft/>
-    <LegRight/>
-    <Blaze>
-      <BlazeInnerMedium/>
-      <BlazeInnerExtra/>
-    </Blaze>
-  </Rocket> 
-)
+const SpaceshipTakeoff = ({ display, onAnimationEnd }) => (
+  <RocketTakeoff display={display} onAnimationEnd={onAnimationEnd.bind(this)}>
+    <LegLeftTakeoff />
+    <LegRightTakeoff />
+    <BlazeTakeoff>
+      <BlazeInnerMedium />
+      <BlazeInnerExtra />
+    </BlazeTakeoff>
+    <RocketSpan />
+    <RocketBow />
+    <FinLeft />
+    <FinRight />
+    <RocketEngine />
+  </RocketTakeoff>
+);
 
-export default Spaceship
-
-
+export { SpaceshipTakeoff };
